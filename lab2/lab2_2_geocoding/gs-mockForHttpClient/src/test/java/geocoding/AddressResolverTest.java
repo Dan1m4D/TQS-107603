17a425d64057310686d5b629ddc6f19ea4dfb8b7
d479 <daniel.madureira@ua.pt>
2024-03-01 02:22:18 +0000

    ex2 done setup 3

--- lab2/lab2_2_geocoding/gs-mockForHttpClient/src/test/java/geocoding/AddressResolverTest.java
+++ lab2/lab2_2_geocoding/gs-mockForHttpClient/src/test/java/geocoding/AddressResolverTest.java
@@ -16,41 +16,57 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.Optional;
 
+import static org.hamcrest.Matchers.nullValue;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.contains;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class AddressResolverTest {
 
+    @Mock
+    ISimpleHttpClient httpClient;
+
+    @InjectMocks
     AddressResolverService resolver;
 
+    @BeforeEach
+    void setUp() {
+        resolver = new AddressResolverService(httpClient);
+    }
+
     @Test
-    @Disabled
     void whenResolveDetiGps_returnJacintoMagalhaeAddress() throws ParseException, IOException, URISyntaxException {
 
         // TODO: implement test; remove Disabled annotation
+        String response = "{\"info\":{\"statuscode\":0,\"copyright\":{\"text\":\"©2024MapQuest,Inc.\",\"imageUrl\":\"http://api.mqcdn.com/res/mqlogo.gif\",\"imageAltText\":\"©2024MapQuest,Inc.\"},\"messages\":[]},\"options\":{\"maxResults\":1,\"ignoreLatLngInput\":false},\"results\":[{\"providedLocation\":{\"latLng\":{\"lat\":40.63436,\"lng\":-8.65616}},\"locations\":[{\"street\":\"AvenidadaUniversidade\",\"adminArea6\":\"Aveiro\",\"adminArea6Type\":\"Neighborhood\",\"adminArea5\":\"Aveiro\",\"adminArea5Type\":\"City\",\"adminArea4\":\"Aveiro\",\"adminArea4Type\":\"County\",\"adminArea3\":\"\",\"adminArea3Type\":\"State\",\"adminArea1\":\"PT\",\"adminArea1Type\":\"Country\",\"postalCode\":\"3810-489\",\"geocodeQualityCode\":\"B1AAA\",\"geocodeQuality\":\"STREET\",\"dragPoint\":false,\"sideOfStreet\":\"L\",\"linkId\":\"0\",\"unknownInput\":\"\",\"type\":\"s\",\"latLng\":{\"lat\":40.63437,\"lng\":-8.65625},\"displayLatLng\":{\"lat\":40.63437,\"lng\":-8.65625},\"mapUrl\":\"\"}]}]}";
+
+        when(httpClient.doHttpGet(anyString())).thenReturn(response);
 
         // will crash for now...need to set the resolver before using it
         Optional<Address> result = resolver.findAddressForLocation(40.63436, -8.65616);
 
-        //return
-        Address expected = new Address( "Avenida da Universidade", "Aveiro","3810-489", "");
+        // return
+        Address expected = new Address("AvenidadaUniversidade", "Aveiro", "3810-489", "");
 
-        assertTrue( result.isPresent());
-        assertEquals( expected, result.get());
- 
+        assertTrue(result.isPresent());
+        assertEquals(expected, result.get());
+
     }
 
     @Test
-    @Disabled
     public void whenBadCoordidates_thenReturnNoValidAddress() throws IOException, URISyntaxException, ParseException {
 
-        ///TODO: implement test
+        /// TODO: implement test
+
+        String invalidResponse = "{\"info\":{\"statuscode\":400,\"copyright\":{\"text\":\"©2024MapQuest,Inc.\",\"imageUrl\":\"http://api.mqcdn.com/res/mqlogo.gif\",\"imageAltText\":\"©2024MapQuest,Inc.\"},\"messages\":[\"Illegalargumentfromrequest:InvalidLatLngspecified.\"]},\"options\":{\"maxResults\":1,\"ignoreLatLngInput\":false},\"results\":[{\"providedLocation\":{},\"locations\":[]}]}";
+        when( httpClient.doHttpGet(anyString())).thenReturn(invalidResponse);
 
 
-        Optional<Address> result = resolver.findAddressForLocation( -361,-361);
+        Optional<Address> result = resolver.findAddressForLocation(-361, -361);
         // verify no valid result
-        assertFalse( result.isPresent());
+        assertFalse(result.isPresent());
 
     }
 }
--- lab2/lab2_2_geocoding/gs-mockForHttpClient/src/test/java/integration/AddressResolverIT.java
+++ lab2/lab2_2_geocoding/gs-mockForHttpClient/src/test/java/integration/AddressResolverIT.java
@@ -4,37 +4,59 @@ import org.json.simple.parser.ParseException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import connection.ISimpleHttpClient;
+import connection.TqsBasicHttpClient;
+import geocoding.Address;
+import geocoding.AddressResolverService;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AddressResolverIT {
 
+    private AddressResolverService resolver;
+    private ISimpleHttpClient httpClient;
 
     @BeforeEach
     public void init(){
+        httpClient = new TqsBasicHttpClient();
+        resolver = new AddressResolverService(httpClient);        
     }
 
 
-    @Disabled
     @Test
     public void whenGoodCoordidates_returnAddress() throws IOException, URISyntaxException, ParseException {
 
         //TODO
 
         // repeat the same tests conditions from AddressResolverTest, without mocks
+        Optional<Address> result = resolver.findAddressForLocation(40.63436, -8.65616);
 
+        // return
+        Address expected = new Address("AvenidadaUniversidade", "Aveiro", "3810-489", "");
+
+        assertTrue(result.isPresent());
+        assertEquals(expected, result.get());
     }
 
-    @Disabled
     @Test
     public void whenBadCoordidates_thenReturnNoValidAddrress() throws IOException, URISyntaxException, ParseException {
 
         //TODO
         // repeat the same tests conditions from AddressResolverTest, without mocks
+
+        Optional<Address> result = resolver.findAddressForLocation(-361, -361);
+        // verify no valid result
+        assertFalse(result.isPresent());
         
     }
 
