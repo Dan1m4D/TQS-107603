50067d2b89447b2b3a7af1fd84df74d4829f5654
d479 <daniel.madureira@ua.pt>
2024-03-08 12:19:53 +0000

    ex 4_1 done

---
+++ lab4/lab_4_1_hello/hello/src/test/java/hello/AppTest.java
@@ -0,0 +1,58 @@
+package hello;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.Logger;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import static java.lang.invoke.MethodHandles.lookup; // Import the necessary class
+import org.slf4j.Logger;
+import static org.slf4j.LoggerFactory.getLogger;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import static org.assertj.core.api.Assertions.assertThat; // Import the necessary class
+
+/**
+ * Unit test for simple App.
+ */
+
+public class AppTest {
+    static final Logger log = getLogger(lookup().lookupClass());
+
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setup() {
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    void test() {
+        // Exercise
+        String sutUrl = "https://bonigarcia.dev/selenium-webdriver-java/";
+        driver.get(sutUrl);
+        String title = driver.getTitle();
+        log.debug("The title of {} is {}", sutUrl, title);
+
+        // Verify
+        assertThat(title).isEqualTo("Hands-On Selenium WebDriver with Java");
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
+
+}
